400c68d041858df91b5174c98a41df6175888966
Jakub Madejczyk <jbmk01@gmail.com>
2024-03-25 01:04:48 +0100

    animations

--- components/Caption.jsx
+++ components/Caption.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
 import { motion, useScroll, useSpring } from "framer-motion";
+import styles from "./caption.module.scss";
 
 import localFont from "next/font/local";
 import { Scroll } from "@react-three/drei";
@@ -12,14 +13,28 @@ export default function Caption() {
 
   // offset [start animation/ end animation] ["ref, window", "ref, window"]
 
+  // const ref = useRef(null);
+  // const { scrollYProgress_2 } = useScroll({
+  //   target: ref,
+  //   offset: ["start 0.75", "center center"],
+  // });
+  // const scaleX_2 = useSpring(scrollYProgress_2);
+
   const ScrollAnimation = ({ children }) => {
     const container = useRef(null);
     const { scrollYProgress } = useScroll({
       target: container,
       offset: ["start 0.75", "center center"],
     });
+    const scaleX = useSpring(scrollYProgress);
+
     return (
-      <motion.div ref={container} style={{ opacity: scrollYProgress }}>
+      <motion.div
+        ref={container}
+        style={{ opacity: scaleX }}
+        whileInView={{ scale: 1.1 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
         {children}
       </motion.div>
     );
@@ -31,21 +46,23 @@ export default function Caption() {
         className={`h-[80vh] bg-black text-white ${myFont.className} flex justify-center items-center`}
       >
         <ScrollAnimation>
-          <p className="text-[9rem] leading-none text-[#F2F2F2]">to make</p>
+          <p className="text-[8rem] leading-none text-[#F2F2F2]">to make</p>
         </ScrollAnimation>
       </div>
+      {/* <div ref={ref}> */}
       <div
         className={`h-[80vh] bg-black text-white ${myFont.className} flex justify-center items-center text-center`}
       >
         <ScrollAnimation>
-          <p className="text-[9rem] leading-none text-[#F2F2F2]">
+          <p className={`text-[8rem] leading-none text-[#F2F2F2] `}>
             {designWord}
             <br />
             Design
           </p>
         </ScrollAnimation>
       </div>
-      <div className="h-[100vh] bg-black"></div>
+      <div className={`bg-black ${styles.container}`}></div>
+      {/* </div> */}
     </>
   );
 }
