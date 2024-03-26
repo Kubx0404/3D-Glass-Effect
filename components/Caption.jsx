7c3801c0efba9a11f80dd6acb3fd3818b3e4453b
Jakub Madejczyk <jbmk01@gmail.com>
2024-03-26 01:46:25 +0100

    unmount animation added

--- components/Caption.jsx
+++ components/Caption.jsx
@@ -1,5 +1,6 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
+
+import { useState, useRef } from "react";
 import {
   motion,
   useScroll,
@@ -14,119 +15,141 @@ import { Scroll } from "@react-three/drei";
 
 const myFont = localFont({ src: "../public/fonts/PPNeueMontreal-Bold.otf" });
 
+const ScrollAnimation = ({ children }) => {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start 0.75", "start 0.6"],
+  });
+  const scaleX = useSpring(scrollYProgress);
+
+  return (
+    <AnimatePresence>
+      <motion.div
+        ref={container}
+        style={{ opacity: scaleX }}
+        className={`sticky top-[40vh] m-auto text-center ${myFont.className} h-auto`}
+        // transition={{ duration:  }}
+        // exit={{ opacity: 0 }}
+        key={children}
+        layout
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
 export default function Caption() {
   const [word, setWord] = useState("");
   const [designWord, setDesignWord] = useState("");
-  const [space, setSpace] = useState(false);
 
   const { scrollY } = useScroll();
 
-  // useEffect(() => {
   useMotionValueEvent(scrollY, "change", (latest) => {
     console.log("Page scroll: ", latest);
-    if (latest < 950) {
-      setSpace(false);
+
+    if (latest < 1000) {
+      // setWord("");
       setWord("");
+      setDesignWord("");
     }
-    if (latest > 950) {
-      setSpace(false);
+    if (latest > 1000) {
+      setDesignWord("");
       setWord("to make");
     }
-    if (latest > 1800) {
-      setSpace(true);
-      setWord("Good Design");
+    if (latest > 2000) {
+      setDesignWord("Good");
+      setWord("Design");
     }
-    if (latest > 2600) {
-      setSpace(true);
-      setWord("Excellent Design");
+    if (latest > 3000) {
+      setDesignWord("Modern");
+      setWord("Design");
     }
+    if (latest > 4000) {
+      setDesignWord("Clean");
+      setWord("Design");
+    }
+    if (latest > 5000) {
+      setDesignWord("");
+      setWord("");
+    }
+
+    // switch (latest) {
+    //   case latest < 1000:
+    //     setWord("");
+    //     setDesignWord("");
+    //     break;
+    //   case latest > 5000:
+    //     setDesignWord("");
+    //     setWord("");
+    //     break;
+    //   case latest > 4000:
+    //     setDesignWord("Clean");
+    //     setWord("Design");
+    //     break;
+    //   case latest > 3000:
+    //     setDesignWord("Modern");
+    //     setWord("Design");
+    //     break;
+    //   case latest > 2000:
+    //     setDesignWord("Good");
+    //     setWord("Design");
+    //     break;
+    //   case latest > 1000:
+    //     setDesignWord("");
+    //     setWord("to make");
+    //     break;
+
+    // default:
+    //   break;
+    // }
   });
-  // }, []);
 
   // offset [start animation/ end animation] ["ref, window", "ref, window"]
 
-  // const ref = useRef(null);
-  // const { scrollYProgress_2 } = useScroll({
-  //   target: ref,
-  //   offset: ["start 0.75", "center center"],
-  // });
-  // const scaleX_2 = useSpring(scrollYProgress_2);
-
-  const ScrollAnimation = ({ children }) => {
-    const container = useRef(null);
-    const { scrollYProgress } = useScroll({
-      target: container,
-      offset: ["start 0.75", "start center"],
-    });
-    const scaleX = useSpring(scrollYProgress);
-
-    return (
-      // <div
-      //   className={`h-[80vh] bg-slate-600 text-white ${myFont.className} flex justify-center items-center text-center w-full border`}
-      //
-      // >
-      <AnimatePresence>
-        <motion.div
-          ref={container}
-          style={{ opacity: scaleX }}
-          whileInView={{ scale: 1.1 }}
-          transition={{ duration: 2, ease: "easeInOut" }}
-          exit={{ rotate: "180deg" }}
-          className={`sticky top-[45vh] m-auto text-center ${myFont.className}`}
-          // layout={true}
-        >
-          {children}
-        </motion.div>
-      </AnimatePresence>
-      // </div>
-    );
-  };
-
   return (
     <>
-      {/* <div
-        className={`h-[80vh] bg-gray-900 text-white ${myFont.className} flex justify-center items-center`}
-      > */}
-      {/* <div
-        className={`h-[200vh] bg-slate-600 text-white ${myFont.className} 
-        
-        w-full border`}
-        // flex justify-center items-center text-center
-      > */}
-      <ScrollAnimation>
-        <p className="text-[8rem] leading-none text-[#F2F2F2] sticky top-2/4">
-          {/* to make */}
-          <AnimatePresence>
-            {space
-              ? word.split(" ").map((word, index) => {
-                  return (
-                    <span key={index}>
-                      {word} <br />
-                    </span>
-                  );
-                })
-              : word.split(" ").map((word, index) => {
-                  return <span key={index}>{word} </span>;
-                })}
-          </AnimatePresence>
-        </p>
-      </ScrollAnimation>
-      {/* </div> */}
-      {/* <div ref={ref}> */}
-      {/* <div
-        className={`h-[80vh] bg-slate-600 text-white ${myFont.className} flex justify-center items-center text-center`}
-      > */}
-      {/* <ScrollAnimation>
-        <p className={`text-[8rem] leading-none text-[#F2F2F2] `}>
-          {designWord}
-          <br />
-          Design
-        </p>
-      </ScrollAnimation> */}
-      {/* </div> */}
-      <div className={`bg-black ${styles.container}`}></div>
-      {/* </div> */}
-      {/* </div> */}
+      <div className={`bg-black h-[450vh]`}>
+        <div className="text-[11rem] leading-none text-[#F2F2F2] sticky top-[40vh]">
+          {/* <AnimatePresence> */}
+          <ScrollAnimation>
+            {designWord && (
+              <motion.span
+                initial={{ opacity: 0 }}
+                whileInView={{ scale: 1.1, opacity: 1 }}
+                // whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                exit={{ opacity: 0 }}
+                // exit={{ rotate: "180deg", opacity: 0 }}
+                key={designWord}
+                // layout
+              >
+                {designWord} <br />
+              </motion.span>
+            )}
+          </ScrollAnimation>
+          {/* </AnimatePresence> */}
+
+          {/* <AnimatePresence> */}
+          <ScrollAnimation>
+            {word && (
+              <motion.span
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, scale: 1.1 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                exit={{ opacity: 0, rotate: "180deg" }}
+                // exit={{ rotate: "180deg", opacity: 0 }}
+                key={word}
+                // layout
+              >
+                {word}
+              </motion.span>
+            )}
+          </ScrollAnimation>
+          {/* </AnimatePresence> */}
+        </div>
+      </div>
     </>
   );
 }
